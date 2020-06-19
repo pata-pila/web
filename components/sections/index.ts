359dedd987db60f73ba34ee538bf38c4af720b3d
Hernan Fuentes Araujo <hernan.fuentes@mulesoft.com>
2020-06-19 10:27:12 -0300

    Add nutrilon slider
    Closes #4

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -2,7 +2,7 @@ import Founder from "./Founder";
 import Tabs from "./Tabs";
 
 export { HomeBannerContainer as Home_banner } from "./home-banner/HomeBanner";
-export { PresentationContainer as Presentation } from "./presentation/Presentation";
+export { PresentationContainer as Presentation } from "./Presentation/Presentation";
 export { ImpactBannerContainer as Icon_list } from "./ImpactBanner/ImpactBanner";
 export { NewsBannerContainer as News_banner } from "./NewsBanner/NewsBanner";
 export { IconTabsContainer as Icon_tabs_section } from "./IconTabs/IconTabs";
