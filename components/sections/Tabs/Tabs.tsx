fab75435eed94fe628eb5d2ed0e20134a7655416
Lautaro Petaccio <lausuper@gmail.com>
2020-07-19 18:03:01 -0300

    Fix build

--- components/sections/Tabs/Tabs.tsx
+++ components/sections/Tabs/Tabs.tsx
@@ -63,7 +63,6 @@ export class Tabs extends PureComponent<Props, { selectedTab: number }> {
                 borderBottomColor: tabs_name_selected_color,
               }}
               onClick={() => this.selectTab(index)}
-              key={index}
             >
               {tab.name}
             </div>
@@ -78,7 +77,6 @@ export class Tabs extends PureComponent<Props, { selectedTab: number }> {
                 color: tabs_content_color,
                 display: this.isSelectedTab(index) ? "block" : "none",
               }}
-              key={index}
             >
               <Text elements={tab.content} />
             </div>
