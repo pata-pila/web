1b2b35f0a233afc195513945716c3f9cddfd6e38
hernaneg350 <hernan.fuentes@mulesoft.com>
2020-07-22 10:32:27 -0300

    Add nutrilon slider (#34)
    Closes #4

---
+++ components/Slider/index.tsx
@@ -0,0 +1,76 @@
+import React, { createRef, FC, useEffect, useState } from "react";
+import { InView } from "react-intersection-observer";
+import classnames from "classnames";
+
+import css from "./Slider.scss";
+
+const Slider: FC<any> = (props) => {
+  // State
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [nextSlide, setNextSlide] = useState(0);
+  const [slideRefs, setSlideRefs] = React.useState([]);
+
+  // Event handlers
+  const onNavTo = (slideIndex) => () => {
+    setNextSlide(slideIndex);
+  };
+  const onScrollBy = (slideIndex) => (scrollingIntoSlide) => {
+    if (scrollingIntoSlide) {
+      setCurrentSlide(slideIndex);
+    }
+  };
+
+  // Effects
+  useEffect(() => {
+    setSlideRefs((elRefs) =>
+      props.children.map((_, i) => elRefs[i] || createRef())
+    );
+  }, [props.children]);
+  useEffect(() => {
+    if (slideRefs[nextSlide]) {
+      slideRefs[nextSlide].current.node.scrollIntoView();
+    }
+  }, [nextSlide]);
+
+  return (
+    <section className={classnames(css.slider, props.className)}>
+      <ul className={css.slides}>
+        {props.children.map((child, thisSlide) => (
+          <InView
+            ref={slideRefs[thisSlide]}
+            threshold={0.5}
+            as="li"
+            onChange={onScrollBy(thisSlide)}
+          >
+            {child}
+          </InView>
+        ))}
+      </ul>
+      {props.children.length > 1 ? (
+        <nav>
+          <ul>
+            {props.children.map((_, thisSlide) => (
+              <li>
+                <label
+                  className={css.sliderNavButton}
+                  onClick={onNavTo(thisSlide)}
+                >
+                  <input
+                    type="radio"
+                    name="slide"
+                    checked={currentSlide === thisSlide}
+                  />
+                  <figure className={css.checkmark} />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      ) : null}
+    </section>
+  );
+};
+
+Slider.displayName = "Slider";
+
+export default Slider;
---
+++ components/sections/Presentation/Presentation.tsx
@@ -0,0 +1,52 @@
+// React/Relay
+import React, { FC } from "react";
+import { createFragmentContainer, graphql } from "react-relay";
+import singleFragmentComponent from "../../../lib/singleFragmentComponent";
+import classnames from "classnames";
+
+// Component
+import { Props } from "./Presentation.types";
+import css from "./Presentation.scss";
+
+// External
+import Slider from "../../Slider";
+import Text from "../../text";
+import { RichText } from "prismic-reactjs";
+
+export const Presentation: FC<Props> = (props) => {
+  return (
+    <section
+      className={classnames(props.theme.toLowerCase(), css.presentation)}
+    >
+      <RichText render={props.title} />
+      <Slider className={css.slider}>
+        {props.slides.map((slide) => (
+          <article
+            className={classnames(
+              css.presentationSlide,
+              css[`text${slide.text_alignment}`]
+            )}
+          >
+            <Text elements={slide.content} />
+          </article>
+        ))}
+      </Slider>
+    </section>
+  );
+};
+
+export const PresentationContainer = createFragmentContainer(
+  singleFragmentComponent(Presentation),
+  {
+    data: graphql`
+      fragment Presentation_data on Presentation {
+        title
+        theme
+        slides {
+          text_alignment
+          content
+        }
+      }
+    `,
+  }
+);
