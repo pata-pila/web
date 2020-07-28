7b79c7e3b8366f1022f9c299c65b4847a2bd3faf
Lautaro Petaccio <lausuper@gmail.com>
2020-07-28 15:01:17 -0300

    Fix index

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,17 +1,10 @@
 import Founder from "./Founder";
 import Tabs from "./Tabs";
-<<<<<<< HEAD
 import Vertical_tabs from "./VerticalTabs";
-
-export { HomeBannerContainer as Home_banner } from "./home-banner/HomeBanner";
-export { PresentationContainer as Presentation } from "./Presentation";
-=======
 import Organizations from "./organizations";
 
 export { HomeBannerContainer as Home_banner } from "./home-banner/HomeBanner";
-export { PresentationContainer as Presentation } from "./Presentation/Presentation";
 export { Organizations };
->>>>>>> 7c8032248c32d6257dbda58d62ae52c8b33f7863
 export { ImpactBannerContainer as Icon_list } from "./ImpactBanner/ImpactBanner";
 export { NewsBannerContainer as News_banner } from "./NewsBanner/NewsBanner";
 export { Founder, Tabs, Vertical_tabs };
