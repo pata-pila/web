bbd7959cb4fe2a2d72a80533e47d78854df1e82f
Tomás Fox <tomas.c.fox@gmail.com>
2020-08-13 15:49:48 -0300

    Add footer content and style it for mobile (#49)
    Desktop style and pata-pila logo are missing.

--- components/sections/Footer/Footer.tsx
+++ components/sections/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 // React/Relay
 import React, { FC } from "react";
 import { createFragmentContainer, graphql } from "react-relay";
-import singleFragmentComponent from "../../../lib/singleFragmentComponent";
+import singleFragmentComponent from "lib/singleFragmentComponent";
 
 // Component
 import { Props } from "./Footer.types";
@@ -9,18 +9,53 @@ import styles from "./Footer.scss";
 
 // External
 import DocumentLink from "components/DocumentLink";
+import { PrismicImage } from "lib/prismicTypes";
 
 const Footer: FC<Props> = (props) => (
-  <footer>
-    <ul>
-      {props.navigation_links.map((link, index) => (
-        <li key={index}>
-          <DocumentLink documentId={link.link._meta.id}>
+  <footer className={styles.footerRoot}>
+    <ul className={styles.actionButtonList}>
+      {props.action_buttons.map((link, index) => (
+        <li key={index} className={styles.actionButtonItem}>
+          <DocumentLink
+            className={styles.actionButton}
+            documentId={link.link._meta.id}
+          >
             {link.text}
           </DocumentLink>
         </li>
       ))}
     </ul>
+    <ul className={styles.navigationLinkList}>
+      {props.navigation_links.map((link, index) => (
+        <li key={index} className={styles.navigationLinkItem}>
+          <DocumentLink
+            className={styles.navigationLink}
+            documentId={link.link._meta.id}
+          >
+            {link.text}
+          </DocumentLink>
+        </li>
+      ))}
+    </ul>
+    <ul className={styles.socialNetworkList}>
+      {props.social_networks.map((socialNetwork, index) => {
+        const image = socialNetwork.image as PrismicImage;
+        return (
+          <li key={index} className={styles.socialNetworkItem}>
+            <a href={socialNetwork.link.url}>
+              <img
+                className={styles.socialNetworkImage}
+                src={image.url}
+                alt={image.alt}
+              />
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+    <small className={styles.copyright}>
+      All rights reserved © 2020 Pata Pila Asociación Civil
+    </small>
   </footer>
 );
 
