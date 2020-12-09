6cbdb83dc58b393d5514e23b9efe70fe333d3d42
Lautaro Petaccio <lausuper@gmail.com>
2020-12-09 09:42:37 -0300

    Change tag script

--- pages/[...site].tsx
+++ pages/[...site].tsx
@@ -117,43 +117,26 @@ class Site extends Component<Props> {
           <title>Pata Pila</title>
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           <script
-            async={true}
-            src="https://www.googletagmanager.com/gtag/js?id=UA-148907387-1"
-          />
-          <script
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-148907387-1');`
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TLX2B8V');`
             }}
           />
           <link
             href="https://fonts.googleapis.com/css?family=Adamina|Nixie+One|Raleway&display=swap"
             rel="stylesheet"
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `!function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '230062738360481');
-      fbq('track', 'PageView');`,
-            }}
-          />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=230062738360481&ev=PageView&noscript=1" />`,
-            }}
-          />
         </Head>
         <main>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLX2B8V"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
           {props.allLandings.edges[0].node.sections
             .filter(({ section }) => section.__typename in sections)
             .map(({ section }, index) => {
